c3d7b8e6b07fd7478209b82ba7b50fc0101d88a3
MohammedFatah <mohammedfatah020@gmail.com>
2026-09-05 19:09:58 +0530

    Add models Venue and Movie

--- src/main/java/com/bookify/BookifyApplication.java
+++ src/main/java/com/bookify/BookifyApplication.java
@@ -3,10 +3,13 @@ package com.bookify;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class BookifyApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone(("Asia/Kokata")));
 		SpringApplication.run(BookifyApplication.class, args);
 	}
 
